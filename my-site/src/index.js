0c23e9a805c2352f4879b000f5dea4af99fa1791
Amy <amyseo@ucla.edu>
2020-10-03 11:28:34 -0700

    deployment

--- my-site/src/index.js
+++ my-site/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from "react-router-dom";
+
+const PUBLIC_URL = "seoamy.github.io";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
 

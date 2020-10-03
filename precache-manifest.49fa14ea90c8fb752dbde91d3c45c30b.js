2d50d7dc985fcbd19aaf18564681aa96175f0ad3
Amy <amyseo@ucla.edu>
2020-10-03 11:01:25 -0700

    Updates

---
+++ precache-manifest.49fa14ea90c8fb752dbde91d3c45c30b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3af1a7a4b33fc4ba338e2c62cbb843",
+    "url": "/seoamy/seoamy.github.io/index.html"
+  },
+  {
+    "revision": "19be2198d92733ad0acb",
+    "url": "/seoamy/seoamy.github.io/static/css/main.f041c395.chunk.css"
+  },
+  {
+    "revision": "cc8b48a081e86ec1bb48",
+    "url": "/seoamy/seoamy.github.io/static/js/2.1f661142.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/seoamy/seoamy.github.io/static/js/2.1f661142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19be2198d92733ad0acb",
+    "url": "/seoamy/seoamy.github.io/static/js/main.3469af7a.chunk.js"
+  },
+  {
+    "revision": "bec54fc8eabb39bcfe82",
+    "url": "/seoamy/seoamy.github.io/static/js/runtime-main.9a890eda.js"
+  },
+  {
+    "revision": "ade78818cc4139cd42c4177a98521446",
+    "url": "/seoamy/seoamy.github.io/static/media/bruinbridge.ade78818.png"
+  },
+  {
+    "revision": "d534a43c37cfcd8846af9d1140297e80",
+    "url": "/seoamy/seoamy.github.io/static/media/delivery.d534a43c.png"
+  },
+  {
+    "revision": "efc64300cfe7eb1fb0840b673d80572a",
+    "url": "/seoamy/seoamy.github.io/static/media/email.efc64300.png"
+  },
+  {
+    "revision": "8f927de54490916b38439880951d5c2f",
+    "url": "/seoamy/seoamy.github.io/static/media/github.8f927de5.png"
+  },
+  {
+    "revision": "ab15f34ff0be9d0a8699fa8642884249",
+    "url": "/seoamy/seoamy.github.io/static/media/instapark.ab15f34f.png"
+  },
+  {
+    "revision": "f074d495e2bcca25a1ab6de786456fae",
+    "url": "/seoamy/seoamy.github.io/static/media/kontagion.f074d495.png"
+  },
+  {
+    "revision": "543a42f2802ef304739f6272cc76165f",
+    "url": "/seoamy/seoamy.github.io/static/media/linkedin.543a42f2.png"
+  },
+  {
+    "revision": "ca7e6592d45e0b1896082e9299825e5b",
+    "url": "/seoamy/seoamy.github.io/static/media/personal-landing.ca7e6592.png"
+  },
+  {
+    "revision": "36c44f6c7a5652805bfc9a6c57f28f5e",
+    "url": "/seoamy/seoamy.github.io/static/media/playmsg.36c44f6c.png"
+  },
+  {
+    "revision": "b4be434322fea421a9bfe38940e0caaf",
+    "url": "/seoamy/seoamy.github.io/static/media/yt.b4be4343.png"
+  }
+]);

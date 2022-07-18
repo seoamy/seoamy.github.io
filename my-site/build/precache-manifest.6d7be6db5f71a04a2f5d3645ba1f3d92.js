b28882672868836622370767b704bb386a025ce0
Amy <amyseo@ucla.edu>
2022-07-18 00:44:30 -0400

    build

---
+++ my-site/build/precache-manifest.6d7be6db5f71a04a2f5d3645ba1f3d92.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a9fadf2ccd660ea02f7af65b794148",
+    "url": "/index.html"
+  },
+  {
+    "revision": "311c12bacdee630d26c1",
+    "url": "/static/css/main.9333c468.chunk.css"
+  },
+  {
+    "revision": "5b6ff15eed5d00cc083e",
+    "url": "/static/js/2.717d1704.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.717d1704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311c12bacdee630d26c1",
+    "url": "/static/js/main.5d4c1011.chunk.js"
+  },
+  {
+    "revision": "c21fe37de23a3f68f6e0",
+    "url": "/static/js/runtime-main.a68b0f98.js"
+  },
+  {
+    "revision": "ade78818cc4139cd42c4177a98521446",
+    "url": "/static/media/bruinbridge.ade78818.png"
+  },
+  {
+    "revision": "af7ce6150647187532b1fcbb37ef7f95",
+    "url": "/static/media/covid-stories.af7ce615.png"
+  },
+  {
+    "revision": "d534a43c37cfcd8846af9d1140297e80",
+    "url": "/static/media/delivery.d534a43c.png"
+  },
+  {
+    "revision": "efc64300cfe7eb1fb0840b673d80572a",
+    "url": "/static/media/email.efc64300.png"
+  },
+  {
+    "revision": "25cbab868f4cc66d0c80caba06a1dfb7",
+    "url": "/static/media/enterprise.25cbab86.png"
+  },
+  {
+    "revision": "8f927de54490916b38439880951d5c2f",
+    "url": "/static/media/github.8f927de5.png"
+  },
+  {
+    "revision": "ab15f34ff0be9d0a8699fa8642884249",
+    "url": "/static/media/instapark.ab15f34f.png"
+  },
+  {
+    "revision": "f074d495e2bcca25a1ab6de786456fae",
+    "url": "/static/media/kontagion.f074d495.png"
+  },
+  {
+    "revision": "09702a418a689b631728a64c05b21677",
+    "url": "/static/media/landing-1.09702a41.png"
+  },
+  {
+    "revision": "543a42f2802ef304739f6272cc76165f",
+    "url": "/static/media/linkedin.543a42f2.png"
+  },
+  {
+    "revision": "36c44f6c7a5652805bfc9a6c57f28f5e",
+    "url": "/static/media/playmsg.36c44f6c.png"
+  },
+  {
+    "revision": "b4be434322fea421a9bfe38940e0caaf",
+    "url": "/static/media/yt.b4be4343.png"
+  }
+]);
